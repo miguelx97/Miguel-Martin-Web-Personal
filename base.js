dda541b51b3090deda90fee5acc3bcecb6e5b668
= <=>
2019-08-05 22:13:54 +0200

    pendiente de poner los links

--- base.js
+++ base.js
@@ -9,13 +9,20 @@ $(document).ready(function () {
     $('#sec-conocimientos').load("sections/conocimientos.html");
     $('#sec-sobre-mi').load("sections/sobre-mi.html");
     $('#footer-container').load("elements/footer.html");
-    
 
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 50){
-            $("#navbar").addClass("bg-dark");
-        } else{
-            $("#navbar").removeClass("bg-dark");
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $("#navbar").addClass("bg-main-color");
+        } else {
+            $("#navbar").removeClass("bg-main-color");
         }
     });
+    $("sec").click(function() {
+        alert("x");
+        $('body').animate({
+            scrollTop: eval($('#' + $(this).attr('target')).offset().top - 70)
+        }, 1000);
+    });
 });
+
